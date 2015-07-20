1c60510f348a54116603a875e560f3caf18a5de2
HPrawns <sgdb999@163.com>
2015-07-20 15:07:55 +0800

    2015-7-20

---
+++ javascipt/obj/Release/Package/PackageTmp/js/Public.js
@@ -0,0 +1,131 @@
+﻿var sheight = 0;
+var swidth = 0;
+var sheightpoint = 0.0;
+//图片地址路径
+var imgurl = [
+{
+    url: "/img/s1.jpg",
+    title: "霜火岭"
+},
+{
+    url: "/img/s2.jpg",
+    title: "霜火岭"
+},
+{
+    url: "/img/s3.jpg",
+    title: "霜火岭"
+},
+{
+    url: "/img/y1.jpg",
+    title: "影月谷"
+},
+{
+    url: "/img/y2.jpg",
+    title: "影月谷"
+},
+{
+    url: "/img/y3.jpg",
+    title: "影月谷"
+}
+]; //地址数组
+var i = 0;
+$(function () {
+    sheight = window.screen.height
+    swidth = window.screen.width;
+
+    sheightpoint = sheight - 95 - 28;          //用于计算浏览器内的实际高度
+
+    
+    MainPicture();              //首页图片加载
+    //$("#picture").css("background", "url(" + imgurl[0].url + ")");      //添加背景图片
+    //$("#picture").css("background-size", "100%");
+
+    // $("#picture").css("background", "url('" + imgurl[0].url + "')");
+
+    $("#LastButton").click(function () {
+        i--;
+        if (i < 0) {
+            i = imgurl.length - 1;
+        }
+        imgcg(i);
+
+    });
+    $("#NextButton").click(function () {
+        i++;
+        if (i >= imgurl.length) {
+            i = 0;
+        }
+        imgcg(i);
+
+    });
+    AddMenu($("#Story"));
+    AddMenu($("#King"));
+    AddMenu($("#NPC"));
+    AddMenu($("#OTHER"));
+
+});
+
+//-----------------------头部下来动态----------------------------------
+
+
+//背景图片切换
+function imgcg(index) {
+    var sWidth = $("#pictures").width(); //获取焦点图的宽度（显示面积）
+    var len = $("#pictures ul li").length; //获取焦点图个数
+
+    $("#pictures ul").css("width", sWidth * (len));
+    var nowLeft = -index * sWidth;
+    $("#pictures ul").stop(true, false).animate({ "left": nowLeft }, 300);
+    index++;
+}
+//首页加载图片
+function MainPicture() {
+    var picdiv = $("#pictures");
+    var picul = $("<ul></ul>");
+    picdiv.append(picul);
+    for (var i = 0; i < imgurl.length; i++) {
+        picul.append($("<li><img src='" + imgurl[i].url + "' /></li>"));
+    }
+    picdiv.append('  <a style="position: inherit; top: 600px; left: 15px" class="picturebtn" id="LastButton">Last</a> <a style="position: inherit; top: 600px; left: 890px" class="picturebtn" id="NextButton">Next</a>');
+}
+document.onmousemove = mouseMove;
+function mouseMove(ev) {
+
+    ev = ev || window.event;
+    var mousePos = mouseCoords(ev);
+    if (parseInt(mousePos.y) > sheightpoint) {
+
+        $("#di").fadeIn("slow");
+    }
+    if (parseInt(mousePos.y) < sheightpoint) {
+        $("#di").fadeOut("slow");
+    }
+}
+function mouseCoords(ev) {
+    if (ev.pageX || ev.pageY) {
+        return { x: ev.pageX, y: ev.pageY };
+    }
+    return {
+        x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+        y: ev.clientY + document.body.scrollTop - document.body.clientTop
+    };
+}
+//背景变色方法
+//对象,新颜色
+function BgColorChange(obj, ncolor) {
+    $(obj).css("background", ncolor);
+}
+//顶部下拉菜单
+function AddMenu(obj) {
+    $(obj).hover(function () {
+        var tops = $(this).offset().top;
+        var lefts = $(this).offset().left;
+        $(this).children('ul').css("top", tops + 36);
+        $(this).children('ul').css("left", lefts);
+        $(this).children('ul').stop(true, true).slideDown();
+        BgColorChange($(obj).children(), "#0F95C0");
+    }, function () {
+        BgColorChange($(obj).children(), "#006592");
+        $(this).children('ul').stop(true, true).slideUp();
+    });
+}
---
+++ javascipt/obj/Release/Package/PackageTmp/jsdata/SHLdata.js
@@ -0,0 +1,34 @@
+﻿var zm1 =
+{
+    title: "霜狼的崛起",
+    url1: "../img/land/霜火岭/WoWScrnShot_011115_140134.jpg",
+    url2: "../img/land/霜火岭/WoWScrnShot_011115_141302.jpg",
+    context: "&nbsp&nbsp&nbsp&nbsp我们在登陆点的峡谷中选择了要塞的根据地" +
+        "在大法师<span class='imptitle' data-tooltip=''>[卡德加]</span>的帮助下开启连接奥格瑞玛的传送门。苦工和地精工程师都陆续传送过来。在收集了一些当地的资源和清理附近一些妨碍我们建设的小戈隆和他们的首领<斯库戈>之后.我们的要塞第一阶段已经建设好了。"
+}
+var zm2 =
+{
+    title: "赞加1",
+    url1: "../img/1407206848739.jpg",
+    url2: "../img/1407206848739.jpg",
+    context: ""
+}
+var zm3 =
+{
+    title: "赞加1",
+    url1: "../img/1407206848739.jpg",
+    url2: "../img/1407206848739.jpg",
+    context: ""
+}
+var zm4 =
+{
+    title: "赞加1",
+    url1: "../img/1407206848739.jpg",
+    url2: "../img/1407206848739.jpg",
+    context: ""
+}
+var datalist = new Array();
+datalist.push(zm1);
+datalist.push(zm2);
+datalist.push(zm3);
+datalist.push(zm4);
