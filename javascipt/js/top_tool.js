fb55a81a8987c9b6e3233f6756b7c9d69b36b203
HPrawns <sgdb999@163.com>
2015-10-27 10:21:32 +0800

    2015-10-27
    详情请参见日志页面

--- javascipt/js/top_tool.js
+++ javascipt/js/top_tool.js
@@ -3,6 +3,24 @@
     var pos = $(".nav").css("position");
     var testtitle = $(".nav");
     testtitle.css("width", document.body.offsetWidth);
+    var scrolls = $(this).scrollTop();
+    if (scrolls > top) {
+        if (window.XMLHttpRequest) {
+            testtitle.css("position", "fixed");
+            testtitle.top = 0;
+            testtitle.css("box-shadow", "1px 1px 1px 1px #888888");
+        }
+        else {
+            testtitle.top = scrolls;
+        }
+    }
+    else {
+        testtitle.css("position", pos);
+        testtitle.top = top;
+        testtitle.css("box-shadow", "0px 0px 0px 0px #888888");
+    }
+
+
     $(window).scroll(function () {
         var scrolls = $(this).scrollTop();
         if (scrolls > top) {
@@ -30,11 +48,11 @@ function AddMenu(obj) {
     $(obj).hover(function () {
         var lefts = $(this).offset().left;
         $(this).children('ul').css("left", lefts);
-        $(this).children('ul').stop(true, true).slideDown();
+        $(this).children('ul').stop(true, false).slideDown();
         BgColorChange($(obj).children(), "#0F95C0");
     }, function () {
         BgColorChange($(obj).children(), "#006592");
-        $(this).children('ul').stop(true, true).slideUp();
+        $(this).children('ul').stop(true, false).slideUp();
     });
 }
 //对象,新颜色
